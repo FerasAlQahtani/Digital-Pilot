1336d4005fed6cae15b76fc536828e89d277c263
Feras AlQahtani <rootfq@gmail.com>
2025-07-14 21:33:41 +0300

    Modefied Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,23 +3,66 @@
 import { useTranslations } from 'next-intl';
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Footer = ({ mini, locale }: { mini?: boolean; locale: string }) => {
   const t = useTranslations("Landing");
+  const pathname = usePathname();
 
+  const links = [
+    { href: `/${locale}`, label: "home", id: "Home" },
+    { href: `/${locale}/Odoo`, label: "Odoo", id: "Odoo" },
+    { href: `/${locale}/services`, label: "services", id: "Services" },
+    { href: `/${locale}/about`, label: "about", id: "About" },
+    { href: `/${locale}/contact`, label: "contact", id: "Contact" },
+  ];
+
+  const column1 = links.filter(link => link.id !== "Contact" && link.id !== "Projects");
+  const column2 = links.filter(link => link.id === "Contact" || link.id === "Projects");
   return (
     <footer className="w-full font-primary bg-gradient-to-r from-blue-600 to-green-600">
       {!mini && (
-        <div
-          className={`w-full px-6 pt-0 pb-0 ${
-            locale === "ar" ? "text-right" : ""
-          }`}
-        >
-          {/* Optional non-mini content */}
+        <div className={`w-full px-6 pt-6 pb-2 ${locale === "ar" ? "text-right" : "text-left"}`}>
+          <div className="flex flex-row gap-8">
+            <div className="flex flex-col gap-2">
+              {column1.map((link) => (
+                <Link
+                  key={link.id}
+                  href={link.href}
+                  className="text-white hover:text-gray-200 duration-100 font-medium"
+                >
+                  {link.id === "Odoo" ? (
+                    <Image
+                      src="/odoo_logo.png"
+                      alt="Odoo"
+                      width={60}
+                      height={20}
+                      className="relative -top-0.5"
+                    />
+                  ) : (
+                    t(link.label)
+                  )}
+                </Link>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              {column2.map((link) => (
+                <Link
+                  key={link.id}
+                  href={link.href}
+                  className="text-white hover:text-gray-200 duration-100 font-medium"
+                >
+                  {t(link.label)}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       )}
 
-      <div className="w-full flex justify-end px-4 pb-0 pt-2">
+      <div className="w-full flex justify-end px-4 pb-0">
         <div className="flex flex-col items-end gap-2">
           <div className="flex flex-row items-end gap-2">
             <Image
