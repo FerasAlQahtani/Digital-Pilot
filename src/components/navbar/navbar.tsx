6458bfd2ff9ab0b4167a9da84ffcc0511b3814a1
Feras AlQahtani <rootfq@gmail.com>
2025-07-14 10:13:57 +0300

    Made logo cliacable and redirect to home page

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -13,12 +13,15 @@ const LandingNavbar = ({ locale }: { locale: string }) => {
   return (
     <nav className={`w-full bg-background font-primary py-7 flex justify-between items-center px-6 md:px-10 xl:px-16 sticky top-0 z-50 dark:border-b shadow-sm ${locale === 'ar' && 'flex-row-reverse'}`}>
 <div className="flex items-center gap-3 mt-1">
-  <Image 
-    src="/logo.png"
-    alt="Cyber Pilot Logo"
-    height={50}
-    width={50}
-  />
+  <Link 
+    href={`/${locale}/Odoo`}    
+  >
+  <img 
+  src="/logo.png" alt="Cyber Pilot Logo" 
+  height={50}
+  width={50}
+   />
+  </Link>
   <h1 className="font-bold -mt-3">Cyber Pilot</h1>
 </div>    
       <div className={`hidden xl:flex items-center gap-8 text-1x2 font-medium text-muted 2xl:absolute 2xl:bottom-[55%] 2xl:right-[50%] 2xl:translate-x-1/2 2xl:translate-y-1/2 ${locale === 'ar' && 'flex-row-reverse'}`}>
