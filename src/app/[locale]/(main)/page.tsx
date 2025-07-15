41b7fd31bb4df25e210cd904bb11745487315ed8
Feras AlQahtani <rootfq@gmail.com>
2025-07-15 13:34:13 +0300

    Modefied Footer 11

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,13 +1,8 @@
 import About from "@/components/about";
 import Services from "@/components/services";
 
-interface Props {
-  params: { locale: string };
-}
-
-const Page = async ({ params }: Props) => {
+const Page = async ({ params }: any) => {
   const { locale } = params;
-
   return (
     <main className="w-full text-foreground bg-background py-16 md:py-20 flex flex-col items-center gap-14 md:gap-20 px-4 sm:6 md:px-12 lg:px-20 xl:px-32 2xl:px-48">
       <Services locale={locale} />
