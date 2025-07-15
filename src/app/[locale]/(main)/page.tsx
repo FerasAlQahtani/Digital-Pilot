de9edf2dc71f18679d1d2a6889b39a24e061e67a
Feras AlQahtani <rootfq@gmail.com>
2025-07-15 13:41:41 +0300

    Modefied Footer 12

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,8 +1,16 @@
 import About from "@/components/about";
 import Services from "@/components/services";
 
-const Page = async ({ params }: any) => {
+// ✅ تعريف نوع واضح بدلًا من any
+interface PageProps {
+  params: {
+    locale: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
   const { locale } = params;
+
   return (
     <main className="w-full text-foreground bg-background py-16 md:py-20 flex flex-col items-center gap-14 md:gap-20 px-4 sm:6 md:px-12 lg:px-20 xl:px-32 2xl:px-48">
       <Services locale={locale} />
@@ -12,4 +20,3 @@ const Page = async ({ params }: any) => {
 };
 
 export default Page;
-
