b7a5cb166d79ef6e4299c6eda0af46571f6a5e8b
Feras AlQahtani <rootfq@gmail.com>
2025-07-15 11:43:40 +0300

    Modefied Footer 6

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,20 +1,21 @@
 import About from "@/components/about";
-import Header from "@/components/header";
 import Services from "@/components/services";
 
-type Params = Promise<{ locale: string }>
+interface PageProps {
+  params: {
+    locale: string;
+  };
+}
 
-const Page = async ({ params }: { params: Params }) => {
-
-  const { locale } = await params;
+const Page = async ({ params }: PageProps) => {
+  const { locale } = params;
 
   return (
     <main className="w-full text-foreground bg-background py-16 md:py-20 flex flex-col items-center gap-14 md:gap-20 px-4 sm:6 md:px-12 lg:px-20 xl:px-32 2xl:px-48">
-      <Header locale={locale} />
       <Services locale={locale} />
       <About locale={locale} />
     </main>
   );
-}
+};
 
 export default Page;
