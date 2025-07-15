3782c3840c178dc2875793ad7502fcfdd6414c79
Feras AlQahtani <rootfq@gmail.com>
2025-07-15 16:31:14 +0300

    Contact form disabled from locale too

--- src/app/[locale]/(main)/contact/page.tsx
+++ src/app/[locale]/(main)/contact/page.tsx
@@ -1,17 +1,17 @@
-import ContactForm from "@/components/contactForm";
-import React from 'react'
+// import ContactForm from "@/components/contactForm";
+// import React from 'react'
 
-type Params = Promise<{ locale: string }>
+// type Params = Promise<{ locale: string }>
 
-const Page = async ({ params }: { params: Params }) => {
+// const Page = async ({ params }: { params: Params }) => {
 
-  const { locale } = await params;
+//   const { locale } = await params;
 
-  return (
-    <main className="w-full text-foreground bg-background py-16 md:py-20 flex flex-col items-center gap-14 md:gap-20 px-4 sm:6 md:px-12 lg:px-20 xl:px-32 2xl:px-48">
-      <ContactForm locale={locale} />
-    </main>
-  )
-}
+//   return (
+//     <main className="w-full text-foreground bg-background py-16 md:py-20 flex flex-col items-center gap-14 md:gap-20 px-4 sm:6 md:px-12 lg:px-20 xl:px-32 2xl:px-48">
+//       <ContactForm locale={locale} />
+//     </main>
+//   )
+// }
 
-export default Page
+// export default Page
